3309fb4ad5e139afcafb1742999d48ea42e18e89
Mat V. <mvincent.it@gmail.com>
2024-12-05 18:36:38 +0100

    📚 Day 5 part 2

--- day5/main.go
+++ day5/main.go
@@ -8,6 +8,7 @@ import (
 	"slices"
 	"strconv"
 	"strings"
+	"time"
 )
 
 const day = "./day5/"
@@ -34,8 +35,10 @@ func main() {
 		}
 	}(file)
 
+	start := time.Now()
 	scanner := bufio.NewScanner(file)
 	sum := 0
+	sumReorder := 0
 	for scanner.Scan() {
 		line := scanner.Text()
 		// Build rules
@@ -43,24 +46,56 @@ func main() {
 			buildElement(line)
 		}
 		// Check
-		safe := true
 		if strings.Contains(line, ",") {
+
 			nbs := strings.Split(line, ",")
-			for idx, nb := range nbs {
-				before := nbs[:idx]
-				iNb, _ := strconv.Atoi(nb)
-				safe = safe && elements[iNb].isNotAfter(before)
-				after := nbs[idx+1:]
-				safe = safe && elements[iNb].isNotBefore(after)
-			}
+			safe := isSafe(nbs)
 			if safe {
-				newOne, _ := strconv.Atoi(nbs[int(len(nbs)/2)])
+				newOne, _ := strconv.Atoi(nbs[(len(nbs) / 2)])
 				sum += newOne
+			} else {
+				for !safe {
+					for idx, nb := range nbs {
+						if safe {
+							break
+						} else {
+							iNb, _ := strconv.Atoi(nb)
+							before := nbs[:idx]
+							for _, b := range before {
+								idxFound := elements[iNb].findMisplaced(nbs, b, false)
+								if idxFound != -1 {
+									backup := nbs[idxFound]
+									nbs[idxFound] = nbs[idx]
+									nbs[idx] = backup
+									break
+								}
+							}
+							safe = isSafe(nbs)
+							if !safe {
+								after := nbs[idx+1:]
+								for _, b := range after {
+									idxFound := elements[iNb].findMisplaced(nbs, b, true)
+									if idxFound != -1 {
+										backup := nbs[idxFound]
+										nbs[idxFound] = nbs[idx]
+										nbs[idx] = backup
+										break
+									}
+								}
+							}
+							safe = isSafe(nbs)
+						}
+					}
+				}
+				newOne, _ := strconv.Atoi(nbs[(len(nbs) / 2)])
+				sumReorder += newOne
 			}
 		}
 	}
 	//fmt.Println(elements)
 	fmt.Println(sum)
+	fmt.Println(sumReorder)
+	fmt.Println(time.Now().UnixMilli() - start.UnixMilli())
 }
 
 func buildElement(line string) {
@@ -89,4 +124,29 @@ func (e Element) isNotAfter(extract []string) bool {
 		}
 	}
 	return true
+}
+
+func isSafe(nbs []string) bool {
+	safe := true
+	for idx, nb := range nbs {
+		before := nbs[:idx]
+		iNb, _ := strconv.Atoi(nb)
+		safe = safe && elements[iNb].isNotAfter(before)
+		after := nbs[idx+1:]
+		safe = safe && elements[iNb].isNotBefore(after)
+	}
+	return safe
+}
+
+func (e Element) findMisplaced(nbs []string, element string, checkBefore bool) int {
+	idx := -1
+	nb, _ := strconv.Atoi(element)
+	toAnalyze := e.after
+	if checkBefore {
+		toAnalyze = e.before
+	}
+	if slices.Contains(toAnalyze, nb) {
+		idx = slices.Index(nbs, element)
+	}
+	return idx
 }
