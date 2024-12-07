548a68aebbcf26a1896fdbd6371ff5c9da842266
Mat V. <mvincent.it@gmail.com>
2024-12-07 22:10:44 +0100

    🔢 Day 7 part 1

---
+++ day7/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+const day = "./day7/"
+const demo = "demo"
+const input = "input"
+const debug = "debug"
+
+func main() {
+	file, err := os.Open(day + input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(file)
+
+	scanner := bufio.NewScanner(file)
+	sum := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		total, _ := strconv.Atoi(strings.Split(line, ":")[0])
+		sNbs := strings.Split(strings.TrimPrefix(strings.Split(line, ":")[1], " "), " ")
+		var nbs []int
+
+		for _, sNb := range sNbs {
+			nbs = append(nbs, toInt(sNb))
+		}
+
+		var results []int
+		computeCombinations(nbs, 1, nbs[0], &results)
+		if slices.Contains(results, total) {
+			sum += total
+		}
+	}
+	fmt.Println(sum)
+}
+
+func computeCombinations(arr []int, index int, acc int, results *[]int) {
+	if index == len(arr) {
+		*results = append(*results, acc)
+		return
+	}
+	// Sum
+	computeCombinations(arr, index+1, acc+arr[index], results)
+	// Multiplication
+	computeCombinations(arr, index+1, acc*arr[index], results)
+}
+
+func toInt(s string) int {
+	nb, _ := strconv.Atoi(s)
+	return nb
+}
