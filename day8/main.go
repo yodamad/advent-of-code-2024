53f73d4edc6df42e27c7a0f6b9bc9e409ec30a3c
Mat V. <mvincent.it@gmail.com>
2024-12-08 22:52:51 +0100

    📶 Day 8 part 2

--- day8/main.go
+++ day8/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"slices"
+	"sort"
 	"strings"
 )
 
@@ -18,8 +19,8 @@ type Point struct {
 	x, y int
 }
 
-var lineId, lineLen, sumAntinodes int
-var antinodes []Point
+var lineId, lineLen int
+var antinodes, resonances []Point
 
 func main() {
 	file, err := os.Open(day + input)
@@ -35,7 +36,6 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	lineId = 0
-	sumAntinodes = 0
 	myMap := make(map[string][]Point)
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -52,11 +52,24 @@ func main() {
 
 	for _, v := range myMap {
 		for p, pv := range v {
+			if len(v) > 1 && !slices.Contains(resonances, pv) {
+				resonances = append(resonances, pv)
+			}
 			computeAntinode(pv, v[0:p])
+			computeResonance(pv, v[0:p])
 			computeAntinode(pv, v[p+1:])
+			computeResonance(pv, v[p+1:])
 		}
 	}
 	fmt.Println(len(antinodes))
+	sort.Slice(resonances, func(i, j int) bool {
+		if resonances[i].y == resonances[j].y {
+			return resonances[i].x < resonances[j].x
+		}
+		return resonances[i].y < resonances[j].y
+	})
+	fmt.Println(resonances)
+	fmt.Println(len(resonances))
 }
 
 func computeAntinode(p Point, pts []Point) {
@@ -66,6 +79,26 @@ func computeAntinode(p Point, pts []Point) {
 		//fmt.Println("(" + strconv.Itoa(newX) + "," + strconv.Itoa(newY) + ")")
 		if newX >= 0 && newX < lineLen && newY >= 0 && newY < lineId && !slices.Contains(antinodes, Point{newX, newY}) {
 			antinodes = append(antinodes, Point{newX, newY})
+		}
+	}
+}
+
+func computeResonance(p Point, pts []Point) {
+	for _, v := range pts {
+		goOn := true
+		round := 1
+		for goOn {
+			newX := p.x - round*(v.x-p.x)
+			newY := p.y - round*(v.y-p.y)
+			//fmt.Println("(" + strconv.Itoa(newX) + "," + strconv.Itoa(newY) + ")")
+			if newX >= 0 && newX < lineLen && newY >= 0 && newY < lineId {
+				if !slices.Contains(resonances, Point{newX, newY}) {
+					resonances = append(resonances, Point{newX, newY})
+				}
+				round++
+			} else {
+				goOn = false
+			}
 		}
 	}
 }
