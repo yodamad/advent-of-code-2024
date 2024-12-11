9a45ecc78bcfd40da97b09d8a26296c14cf1a3f4
Mat V. <mvincent.it@gmail.com>
2024-12-11 13:47:18 +0100

    🗿 Day 11 part 2

--- day11/main.go
+++ day11/main.go
@@ -7,35 +7,77 @@ import (
 )
 
 var nbStones = 0
+var resultPart2 = 224577979481346
 
 func main() {
 	input := "5178527 8525 22 376299 3 69312 0 275"
-	// input := "125 17"
-	nbTurn := 75
+	//input := "125 17"
+	nbTurn := 10000
 	inputs := strings.Split(input, " ")
 	for _, i := range inputs {
-		applyRules(toInt(i), nbTurn)
+		applyRulesPart1(toInt(i), nbTurn)
 	}
-	fmt.Println("----")
+	fmt.Println("-- Part 1 --")
 	fmt.Println(nbStones)
+	fmt.Println("-- Part 2 --")
+	var cache = make(map[int]int)
+	for _, nb := range inputs {
+		cache[toInt(nb)] = 1
+	}
+	for range nbTurn {
+		// fmt.Println(cache)
+		cache = applyRulesPart2(cache)
+	}
+	sum := 0
+	for _, v := range cache {
+		sum += v
+	}
+	fmt.Println(sum)
 }
 
-func applyRules(nb, turn int) {
+func applyRulesPart1(nb, turn int) {
 	if turn == 0 {
 		nbStones++
 		return
 	}
 	if nb == 0 {
-		applyRules(1, turn-1)
+		applyRulesPart1(1, turn-1)
 	} else if len(strconv.Itoa(nb))%2 == 0 {
 		str := strconv.Itoa(nb)
 		mid := len(str) / 2
 		nextTurn := turn - 1
-		applyRules(toInt(str[:mid]), nextTurn)
-		applyRules(toInt(str[mid:]), nextTurn)
+		applyRulesPart1(toInt(str[:mid]), nextTurn)
+		applyRulesPart1(toInt(str[mid:]), nextTurn)
 	} else {
-		applyRules(nb*2024, turn-1)
+		applyRulesPart1(nb*2024, turn-1)
 	}
+}
+
+func applyRulesPart2(stones map[int]int) map[int]int {
+	var updatedStones = map[int]int{}
+
+	update := func(stone, incr int) {
+		if _, v := updatedStones[stone]; !v {
+			updatedStones[stone] = 0
+		}
+		updatedStones[stone] += incr
+	}
+
+	for nb, count := range stones {
+		if nb == 0 {
+			update(1, count)
+		} else if len(strconv.Itoa(nb))%2 == 0 {
+			str := strconv.Itoa(nb)
+			mid := len(str) / 2
+			left := toInt(str[:mid])
+			right := toInt(str[mid:])
+			update(left, count)
+			update(right, count)
+		} else {
+			update(nb*2024, count)
+		}
+	}
+	return updatedStones
 }
 
 func toInt(s string) int {
